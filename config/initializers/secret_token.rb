be2870fcff4189cbab9dc65d51f91000171e0643
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-12-24 00:23:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Distraction::Application.config.secret_key_base = 'a5676b38564054e3efde2de7df20f94fee6f35f3c2053a34e9cf1799dc4a55fb0ecd8fd94477beedb08430809d4fc7d309aeecf7d88fd526f609c959e030af4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Distraction::Application.config.session_store :cookie_store, key: '_distraction_session'
