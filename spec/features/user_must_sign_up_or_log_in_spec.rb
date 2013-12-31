212b0ffc051e5e9f3b3b600a099be95f7164b6c4
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-12-31 10:40:11 -0500

    Design changes, buttons, etc.

--- spec/features/user_must_sign_up_or_log_in_spec.rb
+++ spec/features/user_must_sign_up_or_log_in_spec.rb
@@ -1,6 +1,17 @@
-# As a user
-# I am prompted to log in or sign up on homepage
-# So that I may use the distraction-tracking abilities of the app
+require 'spec_helper'
+
+feature "User must may log in or sign up before accessing core functions", %q{
+	# As a user
+	# I am prompted to log in or sign up on homepage
+	# So that I may use the distraction-tracking abilities of the app
+} do
 
 # *Greeted with sign up/login query
 # *unless they are remembered
+
+context "user is not signed up" do
+  it "log in or sign up" do
+
+
+  end
+end
--- spec/features/user_views_sessions_index_spec.rb
+++ spec/features/user_views_sessions_index_spec.rb
@@ -1,6 +1,10 @@
-# As a User, 
-# I want to see all my sessions on my home page
-# So that I can get a broad view of my progress
+require 'spec_helper'
+
+feature "User views seshes", %q{
+	# As a User, 
+	# I want to see all my sessions on my home page
+	# So that I can get a broad view of my progress
+} do
 
 # *Display email
 # *Display all of User's sessions
@@ -8,3 +12,12 @@
 # *Allow user to specify new session name
 # *and create new session
 
+context "user views seshes" do
+  it "seshes should be displayed" do
+
+    FactoryGirl.create(:sesh)
+    visit root_path
+
+    expect(page).to have_content(seshes)
+  end
+end
