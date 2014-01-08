966f714c6bb87d651e05c981d7f3184e20349bc8
Justus Danny Eapen Jr <justuseapen@gmail.com>
2014-01-08 11:35:45 -0500

    User Sign Up Spec

--- spec/features/user_must_sign_up_or_log_in_spec.rb
+++ spec/features/user_must_sign_up_or_log_in_spec.rb
@@ -10,14 +10,14 @@ feature "User must may log in or sign up before accessing core functions", %q{
 # *unless they are remembered
 # *trying to access other parts of site while not logged in results in erro
 
-scenario "user is not signed up" do
-  visit root_path
-  click_button 'Sign up'
-  fill_in 'Email', with: 'Amanda@chamberlain.st'
-  fill_in 'Password',exactness: true, with: SlamPoet4Lyfe
-  fill_in 'Password confirmation', with: SlamPoet4Lyfe
-  click_button 'Sign up'
+  scenario "user is not signed up" do
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: 'Amanda@chamberlain.st'
+    fill_in 'user_password',exact: true, with: 'SlamPoet4Lyfe'
+    fill_in 'Password confirmation', with: 'SlamPoet4Lyfe'
+    click_button 'Sign up'
 
-  expect(page).to have_content("A message with a confirmation link has been sent to your email address.")
+    expect(page).to have_content("A message with a confirmation link has been sent to your email address.")
   end
 end
