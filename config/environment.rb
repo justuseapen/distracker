a9a48e0dbc797763b0f335f0e9bba36473e61eb7
Justus Danny Eapen Jr <justuseapen@gmail.com>
2014-01-09 13:09:26 -0500

    please fix email

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app20846703@heroku.com'],
-  :password       => ENV['eswmeadr'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
