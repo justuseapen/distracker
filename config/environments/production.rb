7a5b8199f6a902de894b14846337fe4a05aa1e88
Justus Danny Eapen Jr <justuseapen@gmail.com>
2014-01-09 12:34:41 -0500

    Possible fix for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,14 @@ Distracker::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = {:host => 'distracker.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
-  :port    => 25,
-  :domain  => 'distracker.herokuapp.com'
-  }
+  config.action_mailer.default_url_options = { :host => 'distracker.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['app20846703@heroku.com'],
+  :password       => ENV['eswmeadr'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
+ 
 end
